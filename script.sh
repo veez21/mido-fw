a1b067ddd38fb92960460acde97464e2f3da5f9d
Val Mandantes <mandantesv@gmail.com>
2018-04-26 10:43:42 +0000

    improve stuff

--- script.sh
+++ script.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
-user=$(grep user user.pass); user=${user#*=}
-pass=$(grep pass user.pass); pass=${pass#*=}
 
-if [ ! -n "$1" ] || [ "$1" == "-nu" ]; then
-    echo "Getting latest file"
-    link=$(curl -s http://en.miui.com/download-321.html | grep http://bigota.d.miui.com | head -n1 2>/dev/null)
-    link=${link#*href=}; link=${link%%>*}
-    link=$(echo $link | tr -d '"')
-    name=${link##*'/'}
-    echo "Generating $name"
-    if [ -f $name ]; then
-      echo "$name exists"
-    else
-      wget -q --show-progress $link
-    fi
-else
-    name=$1
+user=$(grep user user.pass) &&  pass=$(grep pass user.pass) && { user=${user#*=}; pass=${pass#*=}; }
+
+nu=false
+clean=false
+
+case $1 in
+	-c) clean=true; shift;;
+	-nu) nu=true; shift; name=$1;;
+esac
+
+if [[ ! "$1" ]] || $nu; then
+	echo "Getting latest file"
+	link=$(curl -s http://en.miui.com/download-321.html | grep http://bigota.d.miui.com | head -n1 2>/dev/null)
+	link=${link#*href=}; link=${link%%>*}
+	link=$(echo $link | tr -d '"')
+	name=${link##*'/'}
+	echo "Generating $name"
+	if [[ -f $name ]]; then
+		echo "$name exists"
+	else
+		wget -q --show-progress $link
+	fi
 fi
 
-./create_flashable_firmware.sh $name
+chmod -R a+x $(pwd) && ./xiaomi-flashable-firmware-creator/create_flashable_firmware.sh $name
 
-if [ "$1" != "-nu" ]; then
-  files=$(echo fw*.zip)
-  wput "$files" ftp://$user:$pass@uploads.androidfilehost.com//mido-firmware/
-  echo "Cleaning workspace"
-  rm $files 2>/dev/null
-  rm $name 2>/dev/null
+if ! $nu && [[ "$user" ]] && [[ "$pass" ]]; then
+	files=$(echo fw*.zip | head -n1)
+	wput "$files" ftp://$user:$pass@uploads.androidfilehost.com//mido-firmware/
+fi
+
+if $clean; then
+	echo "Cleaning files"
+	rm $files 2>/dev/null
+	rm $name 2>/dev/null
 fi
